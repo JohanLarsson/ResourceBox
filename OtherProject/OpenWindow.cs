834691d6c7c226d679c07593e06e831583bcb15b
JohanLarsson <johan_gavle1@hotmail.com>
2013-12-17 22:31:27 +0100

    Initial

---
+++ OtherProject/OpenWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ResourcesBox;
+
+namespace OtherProject
+{
+    public class Class1
+    {
+        [Test,RequiresSTA]
+        public void ShowWindowTest()
+        {
+            var window = new MainWindow();
+            var mre = new ManualResetEvent(false);
+            window.Closing += (sender, args) => mre.Set();
+            window.Show();
+            mre.WaitOne();
+        }
+    }
+}
